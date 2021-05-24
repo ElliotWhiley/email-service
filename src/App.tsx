d5ebcd7c182d351f25decc0af8d19a04ea2d6bfe
ElliotWhiley <elliot.whiley94@gmail.com>
2021-05-24 19:47:20 +1200

    Add button to push email event to SQS, which triggers email

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
-import React from "react";
 import "./App.css";
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
+
+async function sendEmail() {
+	try {
+		const message = {
+			recipient: "elliot.whiley94@gmail.com",
+			template: "Pokemon",
+			name: "Elliot",
+		};
+		const params = {
+			MessageBody: JSON.stringify(message),
+			QueueUrl: "",
+			MessageDeduplicationId: Math.floor(Math.random() * 100).toString(),
+			MessageGroupId: Math.floor(Math.random() * 100).toString(),
+		};
+		const command = new SendMessageCommand(params);
+		console.log(command);
+		const creds = {
+			accessKeyId: "",
+			secretAccessKey: "",
+		};
+		const client = new SQSClient({
+			region: "us-west-2",
+			credentials: creds,
+		});
+		console.log(client);
+
+		const data = await client.send(command);
+		console.log(data);
+	} catch (error) {
+		console.log(error);
+	} finally {
+	}
+}
 
 function App() {
-	return <div className="App">hello</div>;
+	return (
+		<div className="App">
+			<button onClick={sendEmail}>Send Email</button>
+		</div>
+	);
 }
 
 export default App;
