c89a3c0d321f0bf603feca2b7ffd7545f488abbf
ElliotWhiley <elliot.whiley94@gmail.com>
2021-05-27 12:57:50 +1200

    Add ability to view email stats: sent, delivered, open rates

--- src/App.tsx
+++ src/App.tsx
@@ -31,103 +31,114 @@ function App() {
 				MessageGroupId: Math.floor(Math.random() * 100).toString(),
 			};
 			const command = new SendMessageCommand(params);
-			console.log(command);
 			const client = new SQSClient({
 				region: "us-west-2",
 				credentials: creds,
 			});
-			console.log(client);
 
-			const data = await client.send(command);
-			console.log(data);
+			await client.send(command);
 		} catch (error) {
 			console.log(error);
 		} finally {
 		}
 	}
 
-	const [emailDeliveryReports, setEmailDeliveryReports] = useState<any[]>([]);
+	const [emailsSent, setEmailsSent] = useState(0);
+	const [emailsDelivered, setEmailsDelivered] = useState(0);
+	const [emailsOpened, setEmailsOpened] = useState(0);
 
 	async function getEmailDeliveryReports(event: any) {
 		event.preventDefault();
 
+		const sent: number = await getEmailDeliveryReportsOfStatus(1);
+		var delivered: number = await getEmailDeliveryReportsOfStatus(2);
+		var opened: number = await getEmailDeliveryReportsOfStatus(3);
+		setEmailsSent(sent);
+		setEmailsDelivered(delivered);
+		setEmailsOpened(opened);
+	}
+
+	async function getEmailDeliveryReportsOfStatus(emailStatus: number) {
 		try {
 			const params = {
 				TableName: "EmailDeliveryReport",
 				ExpressionAttributeValues: {
-					":messageStatusValue": { N: "1" },
+					":emailStatusValue": { N: emailStatus.toString() },
 					":now": { S: new Date().toISOString() },
 				},
 				KeyConditionExpression:
-					"MessageStatus = :messageStatusValue AND CreatedOn < :now",
+					"EmailStatus = :emailStatusValue AND CreatedOn < :now",
 			};
 			const command = new QueryCommand(params);
-			console.log(command);
 			const client = new DynamoDBClient({
 				region: "us-west-2",
 				credentials: creds,
 			});
-			console.log(client);
 
 			const data = await client.send(command);
-			console.log(data);
+			// console.log("******", data);
 			const items: any = data.Items;
-			setEmailDeliveryReports(items);
+			return items.length;
 		} catch (error) {
 			console.log(error);
-		} finally {
 		}
 	}
 
 	return (
-		<>
-			<div className="App">
-				<form onSubmit={sendEmail}>
-					<select
-						value={email}
-						onChange={(event) => setEmail(event.target.value)}
-						name="users"
-						required
-					>
-						<option value="elliot.whiley94@gmail.com">
-							Elliot
-						</option>
-						<option value="kirsten.whittington1993@gmail.com">
-							Kirsten
-						</option>
-					</select>
-					<select
-						value={template}
-						onChange={(event) => setTemplate(event.target.value)}
-						name="users"
-						required
-					>
-						<option value="Pokemon">Pokemon</option>
-						<option value="Greeting">Greeting</option>
-					</select>
-					<input
-						type="text"
-						value={name}
-						onChange={(event) => setName(event.target.value)}
-						placeholder="Name"
-						required
-					/>
-					<button>Send email</button>
-				</form>
+		<div className="App center">
+			<form className="border-bottom" onSubmit={sendEmail}>
+				<select
+					className="pad-right"
+					value={email}
+					onChange={(event) => setEmail(event.target.value)}
+					name="users"
+					required
+				>
+					<option value="elliot.whiley94@gmail.com">Elliot</option>
+					<option value="kirsten.whittington1993@gmail.com">
+						Kirsten
+					</option>
+				</select>
+				<select
+					className="pad-right"
+					value={template}
+					onChange={(event) => setTemplate(event.target.value)}
+					name="users"
+					required
+				>
+					<option value="Pokemon">Pokemon</option>
+					<option value="Greeting">Greeting</option>
+				</select>
+				<input
+					className="pad-right"
+					type="text"
+					value={name}
+					onChange={(event) => setName(event.target.value)}
+					placeholder="Name"
+					required
+				/>
+				<button>Send email</button>
+			</form>
+			<br></br>
+			<button onClick={getEmailDeliveryReports}>Email stats</button>
+			<div>Total emails sent: {emailsSent === 0 ? "?" : emailsSent}</div>
+			<div>
+				Total emails delivered:{" "}
+				{emailsDelivered === 0 ? "?" : emailsDelivered}
 			</div>
 			<div>
-				<button onClick={getEmailDeliveryReports}>Email stats</button>
-				{emailDeliveryReports &&
-					emailDeliveryReports.map((report) => (
-						<ul key={report.CreatedOn.S}>
-							<li>{report.MessageStatus.N}</li>
-							<li>{report.CreatedOn.S}</li>
-							<li>{report.EmailAddress.S}</li>
-							<li>{report.MessageId.S}</li>
-						</ul>
-					))}
+				Total emails opened: {emailsOpened === 0 ? "?" : emailsOpened}
 			</div>
-		</>
+			<div>
+				Open rate:{" "}
+				<b>
+					{emailsOpened === 0 || emailsDelivered === 0
+						? "?"
+						: ((emailsOpened / emailsDelivered) * 100).toFixed(1)}
+					%
+				</b>
+			</div>
+		</div>
 	);
 }
 
